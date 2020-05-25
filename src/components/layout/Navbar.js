96af339aaee94764e1acb7f799b6cebdf934ee90
urjit2498 <urjit64@gmail.com>
2020-05-25 14:04:48 +0530

    Routes changes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,9 +5,11 @@ import { Link } from 'react-router-dom';
 const Navbar = ({ icon, title }) => {
   return (
     <nav className='navbar bg-primary'>
-      <h1>
-        <i className={icon} /> {title}
-      </h1>
+      <Link to='/'>
+        <h1>
+          <i className={icon} /> {title}
+        </h1>
+      </Link>
       <ul>
         <li>
           <Link to='/'>Home</Link>
